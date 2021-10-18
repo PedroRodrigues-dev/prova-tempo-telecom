dbc5d6385cb26ea5f32ef6db4713182f7b7e0905
Pedro Rodrigues <pedroras004@gmail.com>
2021-10-18 17:42:47 -0300

    Declara Rotas Cliente Pedido Produto

--- back/server.js
+++ back/server.js
@@ -1,6 +1,14 @@
 // Modules
 const express = require("express");
 const app = express();
+const clienteRoute = require("./src/routes/clienteRoute");
+const produtoRoute = require("./src/routes/produtoRoute");
+const pedidoRoute = require("./src/routes/pedidoRoute");
+
+// Routes
+app.use("/Clientes", clienteRoute);
+app.use("/Produtos", produtoRoute);
+app.use("/Pedidos", pedidoRoute);
 
 // Others
 const PORT = process.env.PORT || 3000;
