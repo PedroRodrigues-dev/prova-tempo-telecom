5b4dd3015f5aca85005ca234a6ce790730dd632b
Pedro Rodrigues <pedroras004@gmail.com>
2021-10-19 01:26:16 -0300

    Faz validacoes, tratamento de erro 404 e conexao com banco para Produto

--- back/src/controllers/produtoController.js
+++ back/src/controllers/produtoController.js
@@ -1,27 +1,52 @@
+// Module
+const Produto = require("../models/Produto");
+
 // GET ALL
 exports.getAll = (req, res, next) => {
-  res.send("GETALL Produto");
+  Produto.findAll().then((result) => res.json(result));
 };
 
 // GET ONE
 exports.getOne = (req, res, next) => {
   id = req.params.id;
-  res.send("GETONE Produto " + id);
+  Produto.findByPk(id).then((result) => res.json(result));
 };
 
 // POST
 exports.post = (req, res, next) => {
-  res.send("POST Produto");
+  data = req.body;
+  if (data["nome"].trim() && typeof data["valor"] == "number") {
+    Produto.create({
+      nome: data["nome"],
+      valor: data["valor"],
+    });
+    res.sendStatus(200);
+  }
+  res.sendStatus(404);
 };
 
 // PUT
 exports.put = (req, res, next) => {
   id = req.params.id;
-  res.send("PUT Produto " + id);
+  data = req.body;
+  if (data["nome"].trim() && typeof data["valor"] == "number") {
+    Produto.findByPk(id)
+      .then(
+        (result) => (
+          (result.nome = data["nome"]),
+          (result.valor = data["valor"]),
+          result.save()
+        )
+      )
+      .catch((err) => res.sendStatus(404));
+    res.sendStatus(200);
+  }
+  res.sendStatus(404);
 };
 
 // DELETE
 exports.delete = (req, res, next) => {
   id = req.params.id;
-  res.send("DELETE Produto " + id);
+  Produto.destroy({ where: { id: id } });
+  res.sendStatus(200);
 };
