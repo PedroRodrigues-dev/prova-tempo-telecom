8733bc724e18bd25c337d390dec83585a42f804c
Pedro Rodrigues <pedroras004@gmail.com>
2021-10-19 01:26:32 -0300

    Faz validacoes, tratamento de erro 404 e conexao com banco para Cliente

--- back/src/controllers/clienteController.js
+++ back/src/controllers/clienteController.js
@@ -1,27 +1,59 @@
+// Module
+const Cliente = require("../models/Cliente");
+
 // GET ALL
 exports.getAll = (req, res, next) => {
-  res.send("GETALL Cliente");
+  Cliente.findAll().then((result) => res.json(result));
 };
 
 // GET ONE
 exports.getOne = (req, res, next) => {
   id = req.params.id;
-  res.send("GETONE Cliente " + id);
+  Cliente.findByPk(id).then((result) => res.json(result));
 };
 
 // POST
 exports.post = (req, res, next) => {
-  res.send("POST Cliente");
+  data = req.body;
+  if (
+    data["nome"].trim() &&
+    data["telefone"].trim() &&
+    data["data_nascimento"].trim()
+  ) {
+    Cliente.create({
+      nome: data["nome"],
+      telefone: data["telefone"],
+      data_nascimento: data["data_nascimento"],
+    });
+    res.sendStatus(200);
+  }
+  res.sendStatus(404);
 };
 
 // PUT
 exports.put = (req, res, next) => {
   id = req.params.id;
-  res.send("PUT Cliente " + id);
+  data = req.body;
+  if (
+    data["nome"].trim() &&
+    data["telefone"].trim() &&
+    data["data_nascimento"].trim()
+  ) {
+    Cliente.findByPk(id).then(
+      (result) => (
+        (result.nome = data["nome"]),
+        (result.telefone = data["telefone"]),
+        (result.data_nascimento = data["data_nascimento"]),
+        result.save()
+      )
+    );
+  }
+  res.sendStatus(200);
 };
 
 // DELETE
 exports.delete = (req, res, next) => {
   id = req.params.id;
-  res.send("DELETE Cliente " + id);
+  Cliente.destroy({ where: { id: id } });
+  res.sendStatus(200);
 };
